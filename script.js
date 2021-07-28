dfed94affb8467e00b27a0b32ac8635a2affe3f8
RuanFailache <ruanfailache@outlook.com>
2021-07-28 17:00:37 -0300

    update on javascript

--- script.js
+++ script.js
@@ -6,11 +6,26 @@ while (true) {
   qtd = Number(prompt("Por favor digite uma quantia valida"));
 }
 
+const images = [
+  "assets/gif/bobrossparrot.gif",
+  "assets/gif/explodyparrot.gif",
+  "assets/gif/fiestaparrot.gif",
+  "assets/gif/metalparrot.gif",
+  "assets/gif/revertitparrot.gif",
+  "assets/gif/tripletsparrot.gif",
+  "assets/gif/unicornparrot.gif",
+];
+
+let imagesToPlay = images.slice(0, qtd / 2);
+imagesToPlay = imagesToPlay.concat(imagesToPlay);
+
+imagesToPlay = imagesToPlay.sort(() => Math.random() - 0.5);
+
 let createCards = "";
 let counter = 0;
 
 while (counter < qtd) {
-  createCards += '<li class="card"><img src="assets/images/front.png"></li>';
+  createCards += `<li class="card"><img class="front" src="assets/images/front.png"><img class="back hidden" src="${imagesToPlay[counter]}"></li>`;
   counter++;
 }
 
@@ -20,11 +35,9 @@ const cards = document.querySelectorAll(".card");
 
 cards.forEach((card) => {
   card.addEventListener("click", function () {
-    const activeCards = document.querySelectorAll(".cards .active");
+    this.classList.add("active");
 
-    if (activeCards.length < 2) {
-      card.classList.add("active");
-    }
+    document.querySelector(".active .front").classList.add("hidden");
+    document.querySelector(".active .back").classList.remove("hidden");
   });
 });
-
